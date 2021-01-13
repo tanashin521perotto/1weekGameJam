60939fb8fa8aea94d7a6374c97a3def0303309d7
tanashin521perotto <77162032+tanashin521perotto@users.noreply.github.com>
2021-01-14 02:39:03 +0900

    Update Set.cs

---
+++ Assets/Scripts/Set.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Set : MonoBehaviour
+{
+    StageManager stageManager;
+    public static int SetCount = 0;
+    bool isSet;
+
+    private void Start()
+    {
+        stageManager = FindObjectOfType<StageManager>();
+    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (isSet)
+        {
+            return;
+        }
+        if(collision.gameObject.tag == "Block")
+        {
+            SetCount++;
+            isSet = true;
+            SoundManager.instance.PlaySE(0);
+            GameObject ground = Instantiate(stageManager.prefabs[(int)StageManager.TILE_TYPE.GROUND]);
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Block")
+        {
+            SetCount--;
+            isSet = false;
+            SoundManager.instance.PlaySE(1);
+        }
+    }
+}
